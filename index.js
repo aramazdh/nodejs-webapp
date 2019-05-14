6b596dbfcbeef15dd07fdb49ead307b197c8b807
Aramazd <aramazd.dev@gmail.com>
2019-05-14 17:23:28 +0400

    eslint configure

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ function showMessage() {
     const message = "Here there hello everyone!";
     
     for (let i = 0; i < 5; i++) {
-        // eslint-disable-next-line no-console
         console.log(message);
     }
 }
