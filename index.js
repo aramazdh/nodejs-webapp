ee63a519a1d1ff7f4bc64a2d033dabf76a367fa0
Aramazd <aramazd.dev@gmail.com>
2019-05-14 16:53:08 +0400

    initial work

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function showMessage() {
+
+    const message = "Here there hello everyone!";
+    
+    for (let i = 0; i < 5; i++) {
+        console.log(message);
+    }
+}
+
+showMessage();
+
